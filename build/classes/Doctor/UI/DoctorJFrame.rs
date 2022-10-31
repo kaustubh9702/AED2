32e09fb0154df6c5ece2b48bc153beb4432edab7
Kaustubh lawale <kaustubhlawale@gmail.com>
2022-10-30 22:30:18 -0400

    Community updated

---
+++ build/classes/Doctor/UI/DoctorJFrame.rs
@@ -0,0 +1,5 @@
+UI.DoctorJFrame$1
+UI.DoctorJFrame$2
+UI.DoctorJFrame$3
+UI.DoctorJFrame$4
+UI.DoctorJFrame
---
+++ build/classes/Hospital/UI/HospitalJFrame.rs
@@ -0,0 +1,5 @@
+UI.HospitalJFrame
+UI.HospitalJFrame$2
+UI.HospitalJFrame$1
+UI.HospitalJFrame$4
+UI.HospitalJFrame$3
